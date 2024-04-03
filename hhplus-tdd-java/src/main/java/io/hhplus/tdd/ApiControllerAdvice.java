ed9bca196abdc1361ec958e095b43d2394cafae6
jinius <dev.jinius.j@gmail.com>
2024-04-03 12:44:47 +0900

    refactor: update exception

--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -2,6 +2,7 @@ package io.hhplus.tdd;
 
 import io.hhplus.tdd.exception.TddErrorCode;
 import io.hhplus.tdd.exception.TddException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,7 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(),"ERR-99", "에러가 발생했습니다."));
     }
 
     /**
@@ -20,7 +21,11 @@ class ApiControllerAdvice extends ResponseEntityExceptionHandler {
      * @return
      */
     @ExceptionHandler(value = TddException.class)
-    public ResponseEntity<TddErrorCode> handlePointServiceException(TddException e) {
-        return ResponseEntity.status(400).body(e.getTddErrorCode());
+    public ResponseEntity<ErrorResponse> handlePointServiceException(TddException e) {
+        ErrorResponse response = new ErrorResponse(e.getTddErrorCode().getStatus(), e.getTddErrorCode().getCode(), e.getTddErrorCode().getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
+//    public ResponseEntity<TddErrorCode> handlePointServiceException(TddException e) {
+//        return ResponseEntity.status(400).body(e.getTddErrorCode());
+//    }
 }
--- hhplus-tdd-java/src/main/java/io/hhplus/tdd/exception/TddErrorCode.java
+++ hhplus-tdd-java/src/main/java/io/hhplus/tdd/exception/TddErrorCode.java
@@ -3,16 +3,17 @@ package io.hhplus.tdd.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor
 public enum TddErrorCode {
 
-    NOT_FOUND_USER(HttpStatus.BAD_REQUEST, "400", "존재하지 않는 유저입니다."),
-    NOT_ENOUGH_POINT(HttpStatus.BAD_REQUEST, "400", "포인트가 부족합니다.")
+    NOT_FOUND_USER(HttpStatus.BAD_REQUEST.value(), "ERR-01", "존재하지 않는 유저입니다."),
+    NOT_ENOUGH_POINT(HttpStatus.BAD_REQUEST.value(),"ERR-02", "포인트가 부족합니다.")
     ;
 
-    private HttpStatus httpStatus;
+    private int status;
     private String code;
     private String message;
 }
